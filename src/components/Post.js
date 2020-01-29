c83254cdf4bd6b154727dda1b89567a7d5e526a0
alexcologia33 <alexcologia33@gmail.com>
2020-01-29 13:03:52 -0300

    aula 5

--- src/components/Post.js
+++ src/components/Post.js
@@ -127,6 +127,12 @@ export default class Post extends Component {
 							   source={this.carregaIcone(foto.likeada)}/>
 						{this.exibeLikes(foto.likers)}
 						{this.exibeLegenda(foto)}
+						{foto.comentarios.map(comentario => 
+							<View style={styles.comentario} key={comentario.id}>
+								<Text style={styles.tituloComentario}>{comentario.login}</Text>
+								<Text>{comentario.texto}</Text>
+							</View>
+						)}
 					</TouchableOpacity>
 				</View>
             </View>
